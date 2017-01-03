15eaa455a01b832c01ba13575ef70767ba85faee
_mthom <matt@mthompson.org>
2017-01-03 14:30:13 +0000

    Added json

--- js/charts.js
+++ js/charts.js
@@ -1,37 +1,54 @@
-var ctx = document.getElementById("myChart");
+var ctx = document.getElementById("humidity2014");
+$.getJSON("./json/humidity2014.json", function(d)
+                     {
+                         data = d;
+                     });
+// var data = {
+//     labels: ["January", "February", "March", "April", "May", "June", "July",
+//              "August", "September", "October", "November", "December"],
+//     datasets: [
+//         {
+//             label: "Humidity in 2014",
+//             fill: false,
+//             lineTension: 0.1,
+//             backgroundColor: "rgba(75,192,192,0.4)",
+//             borderColor: "rgba(75,192,192,1)",
+//             borderCapStyle: 'butt',
+//             borderDash: [],
+//             borderDashOffset: 0.0,
+//             borderJoinStyle: 'miter',
+//             pointBorderColor: "rgba(75,192,192,1)",
+//             pointBackgroundColor: "#fff",
+//             pointBorderWidth: 1,
+//             pointHoverRadius: 5,
+//             pointHoverBackgroundColor: "rgba(75,192,192,1)",
+//             pointHoverBorderColor: "rgba(220,220,220,1)",
+//             pointHoverBorderWidth: 2,
+//             pointRadius: 1,
+//             pointHitRadius: 10,
+//             data: [65, 59, 80, 81, 56, 55, 40, 50, 33, 44, 55, 88],
+//             spanGaps: false
+//         }
+//     ]
+// };
 var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
+    type: 'line',
+    data: data,
     options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
+        title: {
+            text: "Humidity in 2014",
+            display: true
+        },
+        legend: {
+            display: false
+        },
+        tooltips: {
+            displayColors: false,
+            callbacks: {
+                label: function(tooltipItem, data) {
+                    return data.datasets[0].data[tooltipItem.index];
                 }
-            }]
+            }
         }
     }
 });
