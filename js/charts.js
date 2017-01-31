17c5ab73dc83d1041949e58baa8b420a6cf77051
_mthom <matt@mthompson.org>
2017-01-31 13:24:55 +0000

    Added DM4T blog post

--- js/charts.js
+++ js/charts.js
@@ -1,33 +1,4 @@
-// var data = {
-//     labels: ["January", "February", "March", "April", "May", "June", "July",
-//              "August", "September", "October", "November", "December"],
-//     datasets: [
-//         {
-//             label: "Humidity in 2014",
-//             fill: false,
-//             lineTension: 0.1,
-//             backgroundColor: "rgba(75,192,192,0.4)",
-//             borderColor: "rgba(75,192,192,1)",
-//             borderCapStyle: 'butt',
-//             borderDash: [],
-//             borderDashOffset: 0.0,
-//             borderJoinStyle: 'miter',
-//             pointBorderColor: "rgba(75,192,192,1)",
-//             pointBackgroundColor: "#fff",
-//             pointBorderWidth: 1,
-//             pointHoverRadius: 5,
-//             pointHoverBackgroundColor: "rgba(75,192,192,1)",
-//             pointHoverBorderColor: "rgba(220,220,220,1)",
-//             pointHoverBorderWidth: 2,
-//             pointRadius: 1,
-//             pointHitRadius: 10,
-//             data: [65, 59, 80, 81, 56, 55, 40, 50, 33, 44, 55, 88],
-//             spanGaps: false
-//         }
-//     ]
-// };
-
-function makeChart(data) {
+function makeHums(data) {
     var ctx = document.getElementById("humidity2014");
     var myChart = new Chart(ctx, {
         type: 'line',
@@ -52,10 +23,39 @@ function makeChart(data) {
     });
 }
 
+function makeGas(data) {
+    var ctx = document.getElementById("gas2015");
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: {
+            title: {
+                text: "Gas in 2015",
+                display: true
+            },
+            legend: {
+                display: false
+            },
+            tooltips: {
+                displayColors: false,
+                callbacks: {
+                    label: function(tooltipItem, data) {
+                        return data.datasets[0].data[tooltipItem.index];
+                    }
+                }
+            }
+        }
+    });
+}
+
 function getData() {
     $.getJSON("./json/humidity2014.json", function(d)
               {
-                  makeChart(d);
+                  makeHums(d);
+              });
+    $.getJSON("./json/gas2015.json", function(d)
+              {
+                  makeGas(d);
               });
 }
 
